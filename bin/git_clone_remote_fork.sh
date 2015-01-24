9d5a3c8c03b5cc4b4dbef4cfca308d51d07792ee
Eric Thompson <eric.thompson@puppetlabs.com>
2015-01-24 14:33:07 -0800

    random fixes

--- bin/git_clone_remote_fork.sh
+++ bin/git_clone_remote_fork.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # eg: $1 == cfacter
 git clone https://github.com/er0ck/${1}.git
-if [[ $? != 0 ]]; echo "couldn't find repo for ${1}. did you fork it?"; exit $?; fi
+if [[ $? != 0 ]]; then
+  echo "Couldn't find repo for ${1}. Did you fork it?"
+  exit $?
+fi
 
 cd ${1}
-git remote rename origin er0ck
-git remote add puppet https://github.com/puppetlabs/${1}.git
-git remote set-url --push puppet no-push
-cd ..
+git remote add upstream https://github.com/puppetlabs/${1}.git
+git remote set-url --push upstream no-push
+cd -
