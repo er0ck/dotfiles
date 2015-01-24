8916cc77cc5e4245848b5e0725de9baf573b8c2e
Eric Thompson <eric.thompson@puppetlabs.com>
2015-01-24 14:16:06 -0800

    add install script for dotfiles

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 yep, it's my dotfiles
 =====================
 
-clone me with --recursive to update various submodules included herein
+* clone me with --recursive to update various submodules included herein
+
+* install using install.sh
+    * (backs-up old files; creates a bunch of symlinks into your cloned dir)
---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+backup_file() {
+  BASENAME=`basename $1`
+  echo "backing up .$BASENAME"
+  #echo "$HOME/.$BASENAME $HOME/.$BASENAME.bak"
+  mv -n $HOME/.$BASENAME $HOME/.$BASENAME.bak > /dev/null 2>&1
+}
+
+create_symlink() {
+  echo "symlinking to $1"
+  BASENAME=`basename $1`
+  ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+  #echo "$ABSOLUTE_PATH/$1 $HOME/.$BASENAME"
+  ln -s $ABSOLUTE_PATH/$1 $HOME/.$BASENAME
+}
+
+FILES=(shell/bash_profile shell/bashrc git/gitconfig shell/inputrc vim bin)
+
+for i in ${FILES[@]}; do
+  backup_file ${i}
+  create_symlink ${i}
+done
+
+source $HOME/.bash_profile
+
+exit 0
