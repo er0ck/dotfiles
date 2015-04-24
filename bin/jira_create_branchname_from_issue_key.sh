2fe3bf66847da14e5a470d3416821fe23383b707
Eric Thompson <eric.thompson@puppetlabs.com>
2015-04-24 16:27:59 -0700

    fix shell aliases and functions to use new beaker features
    also adds some more features to jira_create_branchname_from_issue_key
    combine the two sshvm functions into one, it will loginto windows with
    the right user automagically
    provide some fallback to older versions of beaker for showvm when
    sut.log doesn't contain the roles
    make killvm a function and use showvm internally

--- bin/jira_create_branchname_from_issue_key.sh
+++ bin/jira_create_branchname_from_issue_key.sh
@@ -3,4 +3,17 @@
 
 #print the ticket number followed by dash
 TICKET=`echo ${1} | tr '[:lower:]' '[:upper:]'`
-echo "${TICKET}-`jira_get_issue_summary.sh ${1}`" | sed -e 's/ /_/g'
+
+# force to check-ref-format standards, then check it
+# https://www.kernel.org/pub/software/scm/git/docs/git-check-ref-format.html
+BRANCH=`echo "${TICKET}-\`jira_get_issue_summary.sh ${1}\`" | sed -e 's/[ ~^:?*\[\\.]/_/g'`
+echo $BRANCH
+
+# get parent branch name (current branch)
+# git rev-parse --abbrev-ref HEAD
+
+# this requires a leading catagory/hierarchical grouping, which we don't yet support in this script
+#ref=$(git check-ref-format --normalize "maint/$BRANCH") ||
+#die "we do not like '$BRANCH' as a branch name."
+
+#echo $ref
